84b5d7f3208394d3b711e0574065dca724ac4179
Hande Kucukunsal <hkunsal@gmail.com>
2023-01-19 20:41:37 +0300

    Refactored the text_verifier file

--- lib/text_verifier.rb
+++ lib/text_verifier.rb
@@ -1,8 +1,6 @@
 def text_verifier(str)
-  r1 = /\A[A-Z]/
-  r2 = /\z[?!.]/
-  r3 = Regexp.new(r1.source + r2.source)
-  if str.match?(r3) != nil
+  r1 = (/^[A-Z][\s\S]*?\+?[.?!](?:\n\n|$)/)
+  if str.match?(r1) != nil
     true
   end
 end
